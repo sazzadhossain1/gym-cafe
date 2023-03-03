0cc4bd486f70a4c4d1c383c67964a2c4f8af2e98
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-03-04 01:34:37 +0600

    title name done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,13 +3,14 @@ import "./Cart.css";
 
 const Cart = (props) => {
   console.log(props.cart);
-  const { img, name, description, time } = props.cart;
+  const { img, name, description, time, age } = props.cart;
   return (
     <div>
       <div className="cart-info">
         <img src={img} alt="" />
         <h3>{name}</h3>
         <p>{description.slice(0, 300)}</p>
+        <h5>{age}</h5>
         <h5>{time}</h5>
         <button className="add-btn">Add To List</button>
       </div>
--- src/components/GymCart/GymCart.js
+++ src/components/GymCart/GymCart.js
@@ -12,7 +12,17 @@ const GymCart = () => {
   return (
     <div className="gymCart-parent-div">
       <div>
-        <h2 className="title-name">this is gym cart {carts.length}</h2>
+        <div className="title-name">
+          <h2>
+            <small className="g">G</small>ym <small className="c">C</small>afe
+          </h2>
+
+          <img
+            className="title-img"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLcJmaIocmLUfPpkcfmw4LOMBHt6nA_VBfTk3kR6sL6rSf6l2mEpinXQUZWracIwipUjo&usqp=CAU"
+            alt=""
+          />
+        </div>
         <div className="carts-map-div">
           {carts.map((cart) => (
             <Cart key={cart.id} cart={cart}></Cart>
